af2419847c1701c08e2b06f000786be65975e638
Haneul Park <skyp0714@gmail.com>
2025-04-30 14:34:54 -0500

    quest scripts

--- guest-scripts/generate_scripts.sh
+++ guest-scripts/generate_scripts.sh
@@ -11,7 +11,18 @@ echo "Starting DPDK application..."
 dpdk-testpmd -l 0-1 -n 4 -- --nb-cores=1 --coremask=0x02 --forward-mode=macswap --forward-mode=rxptx --proc_times=10 --txd=1024 --rxd=1024 --stats-period=1 &
 
 # Allow some time for the DPDK application to initialize
-sleep 30
+sleep 5
+echo "5 sec past"
+sleep 5
+echo "10 sec past"
+sleep 5
+echo "15 sec past"
+sleep 5
+echo "20 sec past"
+sleep 5
+echo "25 sec past"
+sleep 5
+echo "30 sec past"
 
 echo "Starting benchmark..."
 
@@ -72,7 +83,18 @@ echo "Starting DPDK application..."
 dpdk-testpmd -l 0-1 -n 4 -- --nb-cores=1 --coremask=0x02 --forward-mode=macswap --forward-mode=rxptx --proc_times=10 --txd=1024 --rxd=1024 --stats-period=1 &
 
 # Allow some time for the DPDK application to initialize
-sleep 30
+sleep 5
+echo "5 sec past"
+sleep 5
+echo "10 sec past"
+sleep 5
+echo "15 sec past"
+sleep 5
+echo "20 sec past"
+sleep 5
+echo "25 sec past"
+sleep 5
+echo "30 sec past"
 echo "Starting benchmark..."
 
 # Run Stream benchmark
